e7545895e017ccd264952212bc35ff9dc5027349
Luan Hajzeraj <al.banos1025@googlemail.com>
2016-07-17 22:28:35 +0200

    Add a Methode to generate a .arff-File. As update, about the commit of franck: the class attribute is now a nominal attribute. Now, you can use these .arff-file in the explorer-version of weka with the naive-bayes-classifier

--- src/main/java/unikassel/ct1/preprocessing/Main.java
+++ src/main/java/unikassel/ct1/preprocessing/Main.java
@@ -9,11 +9,7 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.stream.Collectors;
 
 /**
@@ -23,6 +19,7 @@ public class Main {
 
 		//mallo
 		static FastVector      atts1;
+        static FastVector atts2;
 		static Instances       data;
 		static double[]        vals;
 
@@ -65,55 +62,136 @@ public class Main {
         LOG.info("The freuqency is: " + frequency);
 
 
-        //Franck determine frequency
 
-        atts1 = new FastVector();
-        String str = "";
 
-        for (Sample sample : dataSet) {
 
-        	for(unikassel.ct1.preprocessing.Attribute attr : sample){
-        		if (attr instanceof unikassel.ct1.preprocessing.Attribute.DoubleAttribute){
 
-        			str = attr.getValue().toString();
 
-        			str = attr.convertToString();
-        			System.out.println("-----------DoubleAttribute---------------");
-            		System.out.println(str);
-            		atts1.addElement(new Attribute(str));
-        		}
-        		if (attr instanceof unikassel.ct1.preprocessing.Attribute.LabelAttribute){
-        			//LabelAttribute
-        			str = attr.convertToString();
-        			System.out.println("-----------LabelAttribute---------------");
-            		System.out.println(str);
-            		atts1.addElement(new Attribute(str, (FastVector) null));
-        		}
-        		if (attr instanceof unikassel.ct1.preprocessing.Attribute.TimestampAttribute){
-        			//TimestampAttribute
-        			str = attr.convertToString();
-        			System.out.println("-----------TimestampAttribute---------------");
-            		System.out.println(str);
-            		atts1.addElement(new Attribute(str));
-        		}
+        String arffOutput;
 
-        	}
+        //generate the .arff-File about the dataSet
+        arffOutput = generateArffFile(dataSet);
+        //generateArffFile(dataSet);
+
+
+        //Write to an file
+
+        try(  PrintWriter out = new PrintWriter( "./output.arff" )  ){
+            //out.println( data );
+            out.println( arffOutput );
         }
 
 
-        data.add(new Instance(1.0, vals));
 
-        //System.out.println(data);
-
-        ArffSaver saver = new ArffSaver();
-        saver.setInstances(data);
-        try {
-           saver.setFile(new File("./output.arff"));
-           saver.writeBatch();
-       } catch (IOException e) {
-           // TODO Auto-generated catch block
-           e.printStackTrace();
-       }
-        //########################################################################
     }
+
+    /**
+     * generate the .arff-File and save it under the first section in the project (the same section as src)
+
+     * @param dataSet, that have all the data
+     * @return data Instance as String
+     */
+    private static String generateArffFile(DataSet dataSet){
+        //----configure the Attributes
+        //generate an Vector
+
+        //vector for everything else
+        atts1 = new FastVector();
+
+        //vector for the label-data
+        atts2 = new FastVector();
+
+        //possible values for the label-attribute of the data
+        String[] valuesForLabel = {"walking","standing","sit_down_chair_1","sitting_chair_1","stand_up_chair_1"};
+
+        //for each attribute-header in data --> add these to the attribute vector
+        for(String s : dataSet.getHeaders()){
+
+            //We want to classify on label. The class-attribute in weka is a nominal-type-attribute
+            if(s.equals("label")){
+                //atts1.addElement(new Attribute(s,(FastVector)null));
+
+                //for nominals: we write every possible data in an vector and then these to the ats1-vector
+
+                for(String st : valuesForLabel){
+                    atts2.addElement(st);
+                }
+                atts1.addElement(new Attribute(s,atts2));
+
+            }
+            else{
+                atts1.addElement(new Attribute(s));
+            }
+
+        }
+
+        //======================end of configure the Attributes
+
+        //----create instance for every sample
+        data = new Instances("testRelation",atts1,0);
+
+        //======================end create instances
+
+
+        //----fill with values
+        //add values for each sample in the dataset
+
+        for(Sample sa : dataSet){
+
+            //generate for every Sample an seperate value
+            vals = new double[data.numAttributes()];
+
+            for(int j =0; j < dataSet.getHeaders().size(); j++){
+
+                //Check Type of Attribute
+
+                //Timestamp
+                if(sa.getAttribute(j) instanceof unikassel.ct1.preprocessing.Attribute.TimestampAttribute){
+                    vals[j] = (Double) sa.getAttribute(j).getValue();
+                }
+                //Label
+                else if(sa.getAttribute(j) instanceof unikassel.ct1.preprocessing.Attribute.LabelAttribute){
+                    //vals[j] = data.attribute(j).addStringValue( sa.getAttribute(j).getValue().toString() );
+                    vals[j] = atts2.indexOf(sa.getAttribute(j).getValue());
+                }
+                //Double
+                else if(sa.getAttribute(j) instanceof unikassel.ct1.preprocessing.Attribute.DoubleAttribute){
+                    vals[j] = (Double) sa.getAttribute(j).getValue();
+                }
+            }
+
+            /*
+            //first Attribute is a TimeStamp --> a numeric value
+            vals[0] = (Double) sa.getAttribute(0).getValue();
+
+            //second Attribute is a label -->a string value
+            vals[1] = data.attribute(1).addStringValue( sa.getAttribute(1).getValue().toString() );
+
+            //foreach sensor data: write to the vals-array
+            for(int j=2; j < dataSet.getHeaders().size(); j++){
+                vals[j] = (Double) sa.getAttribute(j).getValue();
+            }
+            */
+
+            //write the values to data instance
+            data.add(new Instance(1.0,vals));
+        }
+        return String.valueOf(data);
+
+
+        /*
+        //=======================NEW: build an bayes-model
+
+        //create an empty trainingSet, which have the structure like the .arff-File
+        Instances trainningSet = new Instances("TestTrainninSet",atts1,50);
+
+        //define, which attribute is the class-attribute --> the label-attribute
+        trainningSet.setClassIndex(1);
+
+        //create instances for the trainningSet (for Test: only one)
+        Instance iExample = new
+        */
+
+    }
+
 }
